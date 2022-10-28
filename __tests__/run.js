59cef5a834f9ba818b589099a353877e2303fee2
Ruoja <ruoja@myhost>
2022-10-28 17:59:33 +0300

    literal-test and test runner are added

--- __tests__/run.js
+++ __tests__/run.js
@@ -3,19 +3,45 @@
  */
 
 const { Parser } = require('../src/Parser');
+const assert = require('assert');
+
+/**
+ * List of tests.
+ */
+const tests = [require('./literal-test')];
 
 const parser = new Parser();
 
-const program = `
-  /**
-   * Documentation comment:
-   */
-  "hello";
+/**
+ * For manual tests.
+ */
+function exec() {
+  const program = `
+    /**
+     * Documentation comment:
+     */
+    "hello";
+  
+    // Number
+    42;
+  `;
 
-  // Number
-  42;
-`;
+  const ast = parser.parse(program);
 
-const ast = parser.parse(program);
+  console.log(JSON.stringify(ast, null, 2));
+}
 
-console.log(JSON.stringify(ast, null, 2));
+// Manual test:
+exec();
+
+/**
+ * Test function.
+ */
+function test(program, expected) {
+  const ast = parser.parse(program);
+  assert.deepEqual(ast, expected);
+}
+
+// Run all tests:
+tests.forEach(testRun => testRun(test));
+console.log('All assertions passed!');
