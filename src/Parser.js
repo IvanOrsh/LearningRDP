81c77ffe19e9e668688a9e636e82a19db556e1a7
Ruoja <ruoja@myhost>
2022-10-29 11:18:11 +0300

    added an example of how to support different AST formats to Parser.js

--- src/Parser.js
+++ src/Parser.js
@@ -1,5 +1,82 @@
 const { Tokenizer } = require('./Tokenizer');
 
+//----------------------------------
+// Default AST node factories
+
+const DefaultFactory = {
+  Program(body) {
+    return {
+      type: 'Program',
+      body,
+    };
+  },
+
+  EmptyStatement() {
+    return {
+      type: 'EmptyStatement',
+    };
+  },
+
+  BlockStatement(body) {
+    return {
+      type: 'BlockStatement',
+      body,
+    };
+  },
+
+  ExpressionStatement(expression) {
+    return {
+      type: 'ExpressionStatement',
+      expression,
+    };
+  },
+
+  StringLiteral(value) {
+    return {
+      type: 'StringLiteral',
+      value,
+    };
+  },
+
+  NumericLiteral(value) {
+    return {
+      type: 'NumericLiteral',
+      value,
+    };
+  },
+};
+
+//---------------------------------
+// S-expression AST node factories
+
+const SExpressionFactory = {
+  Program(body) {
+    return ['begin', body];
+  },
+
+  EmptyStatement() {},
+
+  BlockStatement(body) {
+    return ['begin', body];
+  },
+
+  ExpressionStatement(expression) {
+    return expression;
+  },
+
+  StringLiteral(value) {
+    return `${value}`;
+  },
+
+  NumericLiteral(value) {
+    return value;
+  },
+};
+
+const AST_MODE = 'default';
+
+const factory = AST_MODE === 'default' ? DefaultFactory : SExpressionFactory;
+
 class Parser {
   /**
    * Initializes the parser
@@ -34,10 +111,7 @@ class Parser {
    *  ;
    */
   Program() {
-    return {
-      type: 'Program',
-      body: this.StatementList(),
-    };
+    return factory.Program(this.StatementList());
   }
 
   /**
@@ -82,9 +156,7 @@ class Parser {
   EmptyStatement() {
     this._eat(';');
 
-    return {
-      type: 'EmptyStatement',
-    };
+    return factory.EmptyStatement();
   }
 
   /**
@@ -99,10 +171,7 @@ class Parser {
 
     this._eat('}');
 
-    return {
-      type: 'BlockStatement',
-      body,
-    };
+    return factory.BlockStatement(body);
   }
 
   /**
@@ -113,10 +182,7 @@ class Parser {
   ExpressionStatement() {
     const expression = this.Expression();
     this._eat(';');
-    return {
-      type: 'ExpressionStatement',
-      expression,
-    };
+    return factory.ExpressionStatement(expression);
   }
 
   /**
@@ -154,10 +220,8 @@ class Parser {
    */
   StringLiteral() {
     const token = this._eat('STRING');
-    return {
-      type: 'StringLiteral',
-      value: token.value.slice(1, -1), // value between " "
-    };
+    const value = token.value.slice(1, -1);
+    return factory.StringLiteral(value);
   }
 
   /**
@@ -167,10 +231,8 @@ class Parser {
    */
   NumericLiteral() {
     const token = this._eat('NUMBER');
-    return {
-      type: 'NumericLiteral',
-      value: Number(token.value),
-    };
+    const value = Number(token.value);
+    return factory.NumericLiteral(value);
   }
 
   /**
